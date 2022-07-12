d0ab0eb8470566d893325049cfca7414f09bb4d2
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-07-12 18:28:15 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+var count = 0;
+
 function changeColor(){
-  document.getElementById('bg').style.background = 'green';
+  count += 1
+  if (count % 2 == 0) {
+    document.getElementById('bg').style.background = 'linear-gradient(90deg, #a2d7e5, #59a1d9)';
+    document.getElementById('gradientNameH1').innerHTML = "Winter Blue";
+    document.getElementById('colorOne').innerHTML = "#a2d7e5";
+    document.getElementById('colorTwo').innerHTML = "#59a1d9";
+    console.log(count);
+  }
+  else{
+    document.getElementById('bg').style.background = 'linear-gradient(90deg, #ea498b, #59a1d9)';
+    document.getElementById('gradientNameH1').innerHTML = "Cold Evening";
+    document.getElementById('colorOne').innerHTML = "#ea498b";
+    document.getElementById('colorTwo').innerHTML = "#59a1d9";
+    console.log(count);
+  }
+
+
 }
