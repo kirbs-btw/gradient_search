0a95fd039bef414d81294307bd6e507e67379f19
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-07-20 14:13:29 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-//  ideas
-//  think about a gradient generator with the standart methods hue
-
 var count = 0;
 
 // convert hex to decimal
@@ -25,6 +22,7 @@ dataObj = [{"name":"Winter Blue","color_one":"#a2d7e5", "color_two":"#59a1d9"},
 {"name":"Mantle","color_one":"#24C6DC", "color_two":"#514A9D"},
 {"name":"Hard Gradient","color_one":"#000000", "color_two":"#ffffff"}];
 
+dataObj = createJsonGradients();
 
 function lastColor(){
   if ((count) == 0) {
@@ -68,8 +66,9 @@ function textColToLight(){
 
   var lightAvg = (lightValueOne + lightValueTwo) / 2;
 
-  // 195 is a working threshold
-  if (lightAvg > 195) {
+  // 175 is a working threshold
+  // more testing needed 
+  if (lightAvg > 175) {
     var textCol = '#00000030';
     var hexTextCol = '#00000050';
     var buttonCol = '#00000030';
@@ -97,4 +96,100 @@ function getLightLvl(color){
   var b = hexToDecimal(color.slice(4, 6));
 
   return (r + g + b) / 3;
+}
+
+
+// ----------------------------------------------------------------------------------------------------
+// i'm having trouble importing an other js file so here is the rest of the other file
+
+function generateHslColor(){
+  var h = Math.floor(Math.random() * 360);
+  var s = Math.floor(Math.random() * 30 + 60 )
+  var l = Math.floor(Math.random() * 80 + 20)
+
+  return [h, s, l];
+}
+
+function hexToHsl(hex){
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    s = s*100;
+    s = Math.round(s);
+    l = l*100;
+    l = Math.round(l);
+    h = Math.round(360*h);
+
+    return [h, s, l];
+}
+
+function hslToHex(h, s, l) {
+  l /= 100;
+  const a = s * Math.min(l, 1 - l) / 100;
+  const f = n => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color).toString(16).padStart(2, '0');
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+function generateGradient(){
+  var colorOneHsl = generateHslColor();
+  var colorOne = hslToHex(colorOneHsl[0], colorOneHsl[1], colorOneHsl[2]);
+
+  var hChange = Math.floor(Math.random() * 14 + 18);
+  var sChange = Math.floor(Math.random() * 10 + 20);
+  var lChange = Math.floor(Math.random() * 10 + 10);
+
+  var colorTwoValues = [(colorOneHsl[0] + hChange), (colorOneHsl[1] - sChange) ,(colorOneHsl[2] - lChange)];
+  var colorTwo = hslToHex(colorTwoValues[0], colorTwoValues[1], colorTwoValues[2]);
+
+  return [colorOne, colorTwo];
+}
+
+function changeBG(){
+  var colors = generateGradient();
+  var colorOne = colors[0];
+  var colorTwo = colors[1];
+
+  document.getElementById('bg').style.background = `linear-gradient(90deg, ${colorOne}, ${colorTwo})`;
+}
+
+function createJsonGradients(){
+  // creates a Json obj with 100 random gradients
+  var data = [];
+
+  for (var i = 0; i < 100; i++) {
+    var colors = generateGradient();
+    var colorOne = colors[0];
+    var colorTwo = colors[1];
+
+    var newLine = {"name":"Unnamed","color_one":`${colorOne}`, "color_two":`${colorTwo}`};
+    console.log(newLine);
+
+    data.push(newLine);
+  }
+
+  return data;
 }
